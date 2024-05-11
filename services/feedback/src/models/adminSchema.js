c6e624f32216a1ba4fed1ea492246286d3611e4d
harith vithanage <harith.vithanage@inovaitsys.com>
2024-05-11 15:21:40 +0530

    chore : feedback bycrpt removed

--- services/feedback/src/models/adminSchema.js
+++ services/feedback/src/models/adminSchema.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
-const bcrypt = require("bcrypt");
 
 const adminSchema = new mongoose.Schema({
   name: {
@@ -24,32 +23,3 @@ const adminSchema = new mongoose.Schema({
     minlength: [6, "Minimum password length is 6 characters"],
   },
 });
-
-// fire a function before doc saved to db
-adminSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// static method to login admin
-adminSchema.statics.login = async function (email, password) {
-  const admin = await this.findOne({ email });
-  if (admin) {
-    const auth = await bcrypt.compare(password, admin.password);
-    if (auth) {
-      return admin;
-    }
-    throw Error("incorrect password");
-  }
-  throw Error("incorrect email");
-};
-
-module.exports = mongoose.model("admin", adminSchema);
-
-// {
-//   "name" : "admin1",
-//   "role" : "admin",
-//   "email" : "admin@gmail.com",
-//   "password" : "123456"
-// }
--- services/feedback/src/models/instructorSchema.js
+++ services/feedback/src/models/instructorSchema.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
-const bcrypt = require("bcrypt");
 
 const instructorSchema = new mongoose.Schema({
   name: {
@@ -24,32 +23,3 @@ const instructorSchema = new mongoose.Schema({
     minlength: [6, "Minimum password length is 6 characters"],
   },
 });
-
-// fire a function before doc saved to db
-instructorSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// static method to login instructor
-instructorSchema.statics.login = async function (email, password) {
-  const instructor = await this.findOne({ email });
-  if (instructor) {
-    const auth = await bcrypt.compare(password, instructor.password);
-    if (auth) {
-      return instructor;
-    }
-    throw Error("incorrect password");
-  }
-  throw Error("incorrect email");
-};
-
-module.exports = mongoose.model("instructor", instructorSchema);
-
-// {
-//   "name" : "instructor1",
-//   "role" :"instructor",
-//   "email" : "instructor@gmail.com",
-//   "password" : "123456"
-// }
